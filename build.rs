2b1c263d79921afc21b9085ecf03dc590597cced
Hunter Wittenborn <hunter@hunterwittenborn.com>
2023-06-04 01:47:05 -0500

    Fix docs.rs build

--- build.rs
+++ build.rs
@@ -1,9 +1,19 @@
-use std::{env, process::Command};
+use std::path::PathBuf;
+use std::{env, fs, process::Command};
 
 fn main() {
     println!("cargo:rerun-if-changed=src/go");
     env::set_current_dir("src/go").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
+    let out_path = PathBuf::from(&out_dir);
+
+    // The docs.rs builder blocks network access, which would require vendoring everything. I don't
+    // want to go through that hassle right now, so just generate a dummy file to allow the docs to
+    // build.
+    if env::var("DOCS_RS").is_ok() {
+        fs::write(out_path.join("bindings.rs"), "").unwrap();
+        return;
+    }
 
     let status = Command::new("go")
         .args(["build", "-buildmode=c-archive", "-o"])
@@ -38,5 +48,7 @@ fn main() {
         .allowlist_function("HuskSyntaxValidName")
         .generate()
         .unwrap();
-    bindings.write_to_file("src/bindings.rs").unwrap();
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! This crate provides bindings to the [`mvdan.cc/sh` Golang library](https://pkg.go.dev/mvdan.cc/sh).
-mod bindings;
 pub mod shell;
 pub mod syntax;
 pub mod util;
+
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
