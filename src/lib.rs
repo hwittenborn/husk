ae7cdc338a49cb0f4f299e382b6ab9d2c13d6078
Hunter Wittenborn <hunter@hunterwittenborn.com>
2023-06-04 01:54:03 -0500

    Add note about Go toolchain needing to be installed

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 //! This crate provides bindings to the [`mvdan.cc/sh` Golang library](https://pkg.go.dev/mvdan.cc/sh).
+//!
+//! # Note
+//! As this library is a wrapper around a Golang library, you'll need the [Go
+//! toolchain](https://go.dev/) installed on your system in order to build it.
 pub mod shell;
 pub mod syntax;
 pub mod util;
